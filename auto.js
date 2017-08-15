d95fdb8484ccbcc9defd27e54cc42eb0b66fb065
ku ku chong <kukuchong@kudeMacBook-Pro.local>
2017-08-15 16:41:13 +0800

    update auto.js

--- auto.js
+++ auto.js
@@ -12,6 +12,16 @@ const win32Url= {
     'jdk': '',
     'sdk': ''
 }
+
+console.error = function (msg) {
+    console.log('\x1b[31m%s\x1b[31m',`XXX ${msg}`)
+}
+console.success = function (msg) {
+    console.log('\x1b[32m%s\x1b[32m',`√√√ ${msg}`)
+}
+console.info = function (msg) {
+    console.log('\x1b[33m%s\x1b[33m',`!!! ${msg}`)
+}
 const devDependence= {
     'common': [
         {
@@ -68,7 +78,7 @@ function subProcess(dev,cb=false) {
                 if (err.errno === 'ENOENT') {
                     msg = 'Command \'' + dev.command + '\' not found. Is it installed?';
                 }
-                process.stderr.write(msg);
+                console.error(msg);
                 resolve(false);
             }
 
@@ -79,7 +89,7 @@ function subProcess(dev,cb=false) {
                 resolve({stdout,code})
             }else{
                 let msg = ('Command \'' + dev.command+" " +dev.args.join(" ")+ '\' exited with code ' + code);
-                process.stderr.write(msg);
+                console.error(msg);
                 resolve(false)
             }
         })
@@ -91,7 +101,7 @@ function subProcess(dev,cb=false) {
         command.stderr.pipe(process.stdout)
         command.stderr.on('error',err=>{
             let msg= 'Standard error \'' + err.syscall + '\' error: ' + err.stack;
-            process.stderr.write(msg);
+            console.error(msg);
             resolve(false);
         })
 
@@ -104,16 +114,38 @@ class autoMate {
     constructor(type){
         this.platform = type;
         this.env= Object.assign(process.env)
+        this.logFile= path.join(this.env.PWD,'log.json');
+        this.devLog= this.getLog();
     }
     async init(){}
     async installDependence(){
         const devs = devDependence.common.concat(devDependence[this.platform]);
         for(let i=0;i<devs.length;i++){
-            console.log(devs[i].command +" "+ devs[i].args.join(" "));
-            let result= await subProcess(devs[i])
-            if(!result) return false
+            if(!this.devLog[devs[i].args[1]]){
+                console.log(devs[i].command +" "+ devs[i].args.join(" "));
+                let result= await subProcess(devs[i])
+
+                if(!result) {
+                    this.devLog[devs[i].args[1]] = false
+                    return false
+                }else{
+                    this.devLog[devs[i].args[1]] = true;
+                }
+            }
+
+
         }
         return true
+    }
+    getLog(){
+        if(fs.existsSync(this.logFile)){
+            return  JSON.parse(fs.readFileSync(logFile,{encoding:'utf8'}))
+        }else{
+            return {}
+        }
+    }
+    setLog(){
+        fs.writeFileSync(this.logFile,JSON.stringify(this.devLog),{encoding:'utf8'})
     }
 
 }
@@ -122,7 +154,10 @@ class autoMateMac extends autoMate{
         //安装IOS相关依赖
         let result = await this.checkIosEnv()
         if(result){
-        //安装成功可安装安卓相关依赖
+            //替换个编译的文件
+
+            await this.modifyWebDriver()
+            //安装成功可安装安卓相关依赖
             let android= await this.checkAndroidEnv()
 
             if(!android){
@@ -131,6 +166,9 @@ class autoMateMac extends autoMate{
             }
 
             await this.checkAndroidPath();
+
+            //覆盖一个文件
+            await this.modifyWebDriver()
 
             //安装当前环境下的依赖包
 
@@ -146,22 +184,22 @@ class autoMateMac extends autoMate{
     }
     async modifyWebDriver(){
         let module_pwd= await subProcess({'command':'npm',args:['root','-g']})
+        console.log(module_pwd)
         if(module_pwd){
-            let file= await subProcess({'command': 'find',args:[path.join(module_pwd.stdout,'appium'),'-name','UITestingUITests.m']})
+            let file= await subProcess({'command': 'find',args:[path.join(module_pwd.stdout.match(/./g).join(''),'appium'),'-name','UITestingUITests.m']})
 
             if(file&&file.stdout){
-                await subProcess({'command':'cp',args:['-f','./override/UITestingUITests.m',file.stdout]})
+                await subProcess({'command':'cp',args:['-f','./override/UITestingUITests.m',file.stdout.match(/./g).join('')]})
             }
         }
 
-        let file= await subProcess({'command': 'find'})
     }
     async checkIosEnv(){
         //检测 xcode
 
         let xcodeEnable= await this.checkXcode()
         if(!xcodeEnable){
-            process.stderr.write("请先安装Xcode \n")
+            console.error("请先安装Xcode")
             return false;
         }
 
@@ -171,8 +209,10 @@ class autoMateMac extends autoMate{
             //安装依赖
             let isDep= await this.installDependence()
 
+            this.setLog();
+
             if(isDep){
-                console.error("IOS 相关依赖已经安装完毕")
+                console.success("IOS 相关依赖已经安装完毕")
                 return true
             }else {
                 console.error("依赖包安装失败")
@@ -187,23 +227,24 @@ class autoMateMac extends autoMate{
     }
     async checkAndroidEnv(){
         let isSdk= await this.checkSDK();
-        let isAnt= await this.checkAnt()
-        if(isSdk&&isAnt) return true
+        //let isAnt= await this.checkAnt()
+        //if(isSdk&&isAnt) return true
 
-        return false;
+        return isSdk;
     }
     async checkAndroidPath(){
-        let sdkPath= await subProcess({'command':'echo','args':['$JAVA_HOME']})
-        let androidPath= await subProcess({'command':'echo','args':['$ANDROID_HOME']})
-        if(sdkPath===false){
+
+        if(this.env.JAVA_HOME===undefined){
             process.chdir(process.env.HOME)
             if(!fs.existsSync('.bash_profile')){
                 fs.writeFileSync('.bash_profile')
             }
-            fs.appendFileSync('.bash_profile',"\n export JAVA_HOME="+sdkPath.stdout+"\n export PATH=$JAVA_HOME/bin:$PATH",'utf8')
+            let jdkPath = await subProcess({'command':'/usr/libexec/java_home','args':['-V']})
+            //console.log(jdkPath)
+            fs.appendFileSync('.bash_profile',"\n export JAVA_HOME="+jdkPath.stdout.match(/./g).join('')+"\n export PATH=$JAVA_HOME/bin:$PATH",'utf8')
         }
-        if(androidPath===false){
-            fs.appendFileSync('.bash_profile',"\n export ANDROID_HOME="+androidPath.stdout,'utf8')
+        if(this.env.ANDROID_HOME===undefined){
+            fs.appendFileSync('.bash_profile',"\n export ANDROID_HOME="+path.join(this.env.HOME,'Library/Android/sdk/'),'utf8')
         }
         let sourceCommand= await subProcess({'command':'source','args':['.bash_profile']})
 
@@ -220,19 +261,35 @@ class autoMateMac extends autoMate{
     }
     async checkSDK(){
         let dev= {
-            'command': "java",
-            'args': ['-version']
+            'command': "android",
+            'args': ['-h']
         }
         try {
             let result= await subProcess(dev,true)
             return result;
         }catch(err){
             if(err.errno === 'ENOENT'){
+                let tmpFile= path.join(process.env.HOME,'android-studio.dmg')
                 let dev= {
-                    'command': "brew",
-                    'args': ['tap','install','java']
+                    'command': "curl",
+                    'args': ['https://dl.google.com/dl/android/studio/install/2.3.3.0/android-studio-ide-162.4069837-mac.dmg','-o', tmpFile]
                 }
                 let res= await subProcess(dev)
+
+                if(res){
+
+                    console.success(`android studio 已经下载到 ${tmpFile}`)
+                    console.info('安装 android-studio 过程中不要修改SDK的安装路径')
+                    let mount= await subProcess({
+                        'command': "hdiutil",
+                        'args': ['mount',tmpFile]
+                    })
+                    console.error(`如果studio 没有自动安装成功!!,请手动安装,文件路径: ${tmpFile}`)
+
+                    return mount
+                }
+
+
                 return res
             }
         }
@@ -272,7 +329,7 @@ class autoMateMac extends autoMate{
             else
                 return false
         }catch(err){
-            process.stderr.write(err.message)
+            console.error(err.message)
             return false;
         }
     }
@@ -305,9 +362,30 @@ class autoMateMac extends autoMate{
 class autoMateWin extends autoMate{
     constructor(type){
         super(type)
-        this.SDKEnable=false;
+        this.SDKEnable= false;
+        this.envCheck= [];
+
     }
     async init(){
+        let isDep= await this.installDependence()
+        this.setLog()
+        if(!isDep) {
+            console.error("依赖包安装失败")
+            process.exit()
+        }
+
+        await this.checkJDK()
+
+        await this.checkAnt()
+
+        await this.checkMaven()
+
+        await this.checkSDk()
+
+        if(this.envCheck.some(elem=>!elem.exist)){
+            process.exit()
+        }
+        await subProcess({'command':'appium',args:[]})
 
     }
     async checkJDK(){
@@ -318,48 +396,84 @@ class autoMateWin extends autoMate{
 
         try {
             let t= await subProcess(dev,true)
-
+            this.envCheck.push({
+                'env':'JDK',
+                'exist': true
+            })
         }catch (err){
             if(err.errno==='ENOENT'){
-                let d = await subProcess({
-                    'command': 'curl',
-                    'args': [win32Url.jdk,'-C','-o','jdk.zip','--progress']
+                this.envCheck.push({
+                    'env':'JDK',
+                    'exist': false
                 })
-                if(d){
-                    
-                }
 
             }
         }
 
 
     }
-    async checkAndroidSDK(){
 
-    }
     async checkAnt(){
 
+        if(this.env.Path.indexOf('ant')== -1){
+            console.error("系统未检测到Ant,请下载ant 并添加到path 环境变量")
+            this.envCheck.push({
+                'env':'ant',
+                'exist': false
+            })
+        }else{
+            this.envCheck.push({
+                'env':'ant',
+                'exist': true
+            })
+        }
     }
 
     async checkMaven(){
+        let dev= {
+            'command': 'mvn',
+            'args': ['-version']
+        }
 
+        try {
+            let t= await subProcess(dev,true)
+
+            this.envCheck.push({
+                'env':'Maven',
+                'exist': true
+            })
+        }catch (err){
+            if(err.errno==='ENOENT'){
+                this.envCheck.push({
+                    'env':'Maven',
+                    'exist': false
+                })
+
+                console.error('系统未检测到maven,请下载maven 并设置M2HOME 和 M2 环境变量')
+
+            }
+        }
     }
     async checkSDk(){
         let dev= {
-            'command': "java",
-            'args': ['-version']
+            'command': "adb",
+            'args': ['version']
         }
         try {
             let result= await subProcess(dev,true)
-            return result;
+
+            this.envCheck.push({
+                'env':'SDK',
+                'exist': true
+            })
+
         }catch(err){
             if(err.errno === 'ENOENT'){
-                let dev= {
-                    'command': "brew",
-                    'args': ['tap','install','java']
-                }
-                let res= await subProcess(dev)
-                return res
+                this.envCheck.push({
+                    'env':'SDK',
+                    'exist': false
+                })
+                console.error('系统未检测到Android SDK,请下载并安装Android Studio 并设置ANDROID_HOME 环境变量 并将 platform-tools 和 tools 添加到 path 环境变量中 ')
             }
         }
     }
